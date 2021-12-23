6d928c327538d7cbd6ae9c690b6b8579f8901d71
Pawan Singh <55322511+brigadierpratap@users.noreply.github.com>
2021-12-24 01:22:58 +0530

    Update GetAllCountries.js

--- functions/GetAllCountries.js
+++ functions/GetAllCountries.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 module.exports= async()=>{
     try {
-        const res= await axios.get('https://vax.hyperx.cloud/countries')
+        const res= await axios.get('https://countrystatecityapi.herokuapp.com/')
         return res.data
 
     } catch (error) {
